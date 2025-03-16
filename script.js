973a96da194e16178b42547738fc32e507fbab5c
Grace <gracegodson.o@gmail.com>
2025-03-16 20:03:37 +0100

    modified some files

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-document.getElementById('upload-button').addEventListener('click', function() {
-    const projectLink = prompt("Enter the link to your project:");
-    if (projectLink) {
-        const li = document.createElement('li');
-        li.textContent = projectLink;
-        document.getElementById('project-list').appendChild(li);
-    } else {
-        alert("No link entered.");
-    }
-});
+// document.getElementById('upload-button').addEventListener('click', function() {
+//     const projectLink = prompt("Enter the link to your project:");
+//     if (projectLink) {
+//         const li = document.createElement('li');
+//         li.textContent = projectLink;
+//         document.getElementById('project-list').appendChild(li);
+//     } else {
+//         alert("No link entered.");
+//     }
+// });
+
+//script for an active nav bar
+document.addEventListener("DOMContentLoaded", function(){
+    let currentLocation = window.location.href;
+    let menuItems = document.querySelectorAll("nav ul li a");
+    menuItems.forEach(item => {
+        if (item.href === currentLocation){
+            item.classList.add("active")
+        }
+    })
+
+    //js for a responsive menu links//
+//     document.querySelector(".menu-icon").addEventListener("click", function(){
+//     const menuLinks = document.querySelector("#menu-links");
+//     const icon = document.querySelector("#bars");
+//     icon.classList.toggle("fa-bar");
+//     menuLinks.classList.toggle("show");
+   
+// })
+
+})
